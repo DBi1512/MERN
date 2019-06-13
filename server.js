def693dbb5c3470e6299599a8a13bf747322653c
Dean Phan <dean.t.phan@gmail.com>
2019-06-13 11:54:00 +0300

    delete yarn

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 8000;
 const studentRoute = require("./server/routes/student-routes");
 const mongoose = require("mongoose");
@@ -17,7 +18,11 @@ mongoose.connect(process.env.MONGODB_URI, err => {
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1.0", studentRoute);
- 
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
